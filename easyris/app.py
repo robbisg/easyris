bfcb32d722c23039d5d46fd280ff314869035bb5
iPadawan <riccardo.navarra@gmail.com>
2016-03-16 16:24:42 +0100

    Say hello to HTTPS.

--- easyris/app.py
+++ easyris/app.py
@@ -3,6 +3,17 @@ from utils.api import cities
 from patient.api import patient
 from flask.ext.cors import CORS
 
+
+#import cryptography
+#from OpenSSL import crypto
+#from OpenSSL import SSL
+#import os
+#context = SSL.SSLContext(SSL.PROTOCOL_TLSv1_2)
+#context.load_cert_chain('/etc/webserver-ssl/webserver.crt','/etc/webserver-ssl/webserver.key')
+#cer = os.path.join(os.path.dirname(__file__), '/etc/webserver-ssl/webserver.crt')
+#key = os.path.join(os.path.dirname(__file__), '/etc/webserver-ssl/webserver.key')
+ 
+
 app = Flask(__name__)
 app.register_blueprint(patient, url_prefix='/patient')
 app.register_blueprint(cities, url_prefix='/cities')
@@ -15,7 +26,13 @@ def entry():
 
 if __name__ == '__main__':
     #app.debug = True
+    
+#    context = ( cer, key )
+
     app.run(host='0.0.0.0', 
             port=5000, 
             debug=True,
-            threaded=True)
+            threaded=True,
+		ssl_context=('/etc/webserver-ssl/webserver.crt', '/etc/webserver-ssl/webserver.key')
+ #           ssl_context=context
+		)
